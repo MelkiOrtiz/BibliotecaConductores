d3ea9170ce35d3e31489189ef57cb36781683a33
Melki <mortizm28@miumg.edu.gt>
2024-07-30 10:32:28 -0600

    prueba

--- src/main/java/umg/ejercicios/conductores/ConductorDeCoche.java
+++ src/main/java/umg/ejercicios/conductores/ConductorDeCoche.java
@@ -1,2 +1,16 @@
-package umg.ejercicios.conductores;public class ConductorDeCoche {
+package umg.ejercicios.conductores;
+
+public class ConductorDeCoche extends Conductor {
+    private int experienciaAños;
+
+    public ConductorDeCoche(String nombre, String licencia, int experienciaAños) {
+        super(nombre, licencia);
+        this.experienciaAños = experienciaAños;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Experiencia (años): " + experienciaAños);
+    }
 }
